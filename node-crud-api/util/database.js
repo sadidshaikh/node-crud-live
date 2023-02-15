255f025d140e6a0c24bc46a3504262fb52173eab
sadidshaikh <sadid.22010049@viit.ac.in>
2023-02-15 19:23:50 +0530

    final commit

--- node-crud-api/util/database.js
+++ node-crud-api/util/database.js
@@ -1,8 +1,15 @@
 import Sequelize from "sequelize";
+import { config } from "dotenv";
+config();
 
-const sequelize = new Sequelize("node-crud-live", "postgres", "1234", {
-  host: "localhost",
-  dialect: "postgres",
-});
+const sequelize = new Sequelize(
+  process.env.PG_DB,
+  process.env.PG_USER,
+  process.env.PG_PASSWORD,
+  {
+    host: "localhost",
+    dialect: "postgres",
+  }
+);
 
 export default sequelize;
